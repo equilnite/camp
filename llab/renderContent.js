40919d417eaa27ff6b30a2575d390a566ef676fd
Edward Chang <equilnite@gmail.com>
2023-06-07 16:45:56 -0700

    fix snap link?

--- llab/renderContent.js
+++ llab/renderContent.js
@@ -59,7 +59,7 @@ getSnapRunURL = function(targeturl, options) {
         finalurl = snapRunURLBaseVersion.replace('VERSION', options.version);
     }
 
-    var currdom = "https://csp.echa.ng";
+    var currdom = "https://camp.echa.ng";
     if (currdom == "localhost") {
         currdom = 'http://' + currdom + ":" + window.location.port;
         // finalurl = finalurl.replace('https://snap', 'http://extensions.snap');
